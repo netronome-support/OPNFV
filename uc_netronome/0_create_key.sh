09cfbd3a147ffb51b26e7ba0616c472da53e9bfd
Mario <root@winterhoek.netronome.com>
2017-10-04 11:28:50 +0200

    added uc scripts

---
+++ uc_netronome/0_create_key.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+. $HOME/adminrc
+
+script_dir="$(dirname $(readlink -f $0))"
+
+cd $script_dir
+
+if [ -f markey.pem ]; then
+        echo -e "${GREEN}markey.pem already exists${NC}"
+	echo "Exiting"
+	exit 1	
+fi
+
+openstack keypair create markey > markey.pem
+chmod 600 markey.pem
+openstack keypair list
+
