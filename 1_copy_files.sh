0389ba2738ceff9c8e7fc1b40452ec1ea703c73c
Mario <root@winterhoek.netronome.com>
2017-10-04 12:38:00 +0200

    fixed undercloud ip awk

--- 1_copy_files.sh
+++ 1_copy_files.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mac=`virsh dumpxml undercloud | grep br-admin -B 1 | awk -F "'" 'NR==1 {print $2}'`
-ip=`arp -an | awk -F '[()]' '/'$mac'/ {print $2}'`
-echo $ip
+#mac=`virsh dumpxml undercloud | grep br-admin -B 1 | awk -F "'" 'NR==1 {print $2}'`
+#ip=`arp -an | awk -F '[()]' '/'$mac'/ {print $2}'`
+ip=`virsh net-dhcp-leases default | awk '/undercloud/ {print $5}' | cut -d"/" -f1`
+echo "Copying uc_netronome to $ip:/home/stack/"
 
 script_dir="$(dirname $(readlink -f $0))"
 
