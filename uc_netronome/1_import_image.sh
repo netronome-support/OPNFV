1d8ce33dcc93ec1a1d528310e55f245286d81d6d
Mario <Mario>
2017-10-05 16:24:18 +0200

    update generate_pktgen_cmd

--- uc_netronome/1_import_image.sh
+++ uc_netronome/1_import_image.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-. $HOME/adminrc
-
+. $HOME/adminrc 
 echo "Importing netronome_perf"
 script_dir="$(dirname $(readlink -f $0))"
-
 glance image-create --name netronome_perf --file $script_dir/netronome_perf.img --disk-format qcow2 --container-format bare --progress --visibility public
 
 openstack image list | grep netronome_perf
--- uc_netronome/7_generate_pktgen_cmds.sh
+++ uc_netronome/7_generate_pktgen_cmds.sh
@@ -10,24 +10,26 @@ if [ -z "$2" ]; then
  exit -1
 fi
 
+. $HOME/adminrc
 
 echo -----------------------------------
 echo Host0: $1
 echo -----------------------------------
 
-ip0=`nova interface-list $1 | awk 'NR > 4 {print $8}'`
-mac0=`nova interface-list $1 | awk 'NR > 4 {print $10}'`
+ip0=`openstack server list | awk '/'"$1"'/{print $9}' | cut -d',' -f1`
+mac0=`nova interface-list $1 | awk '/'"$ip0"'/ {print $10}'`
 
 echo IPv4 address: $ip0
 echo MAC address: $mac0
 echo -----------------------------------
 
 echo -----------------------------------
-echo Host0: $2
+echo Host1: $2
 echo -----------------------------------
 
-ip1=`nova interface-list $2 | awk 'NR > 4 {print $8}'`
-mac1=`nova interface-list $2 | awk 'NR > 4 {print $10}'`
+ip1=`openstack server list | awk '/'"$2"'/{print $9}' | cut -d',' -f1`
+mac1=`nova interface-list $2 | awk '/'"$ip1"'/ {print $10}'`
+
 
 echo IPv4 address: $ip1
 echo MAC address: $mac1
@@ -41,9 +43,9 @@ set 0 src ip $ip0/24
 set 0 dst ip $ip1
 
 Host1:
-set 1 dst mac $mac0
-set 1 src ip $ip1/24
-set 1 dst ip $ip0
+set 0 dst mac $mac0
+set 0 src ip $ip1/24
+set 0 dst ip $ip0
 -----------------------------------
 
 "
