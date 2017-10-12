5055b64658e897056c6c4f324906467a369a6b7f
mario <mario>
2017-10-12 05:27:08 +0000

    removed flavor mod

--- ys_netronome/0_ys_configure_nfp_image.sh
+++ ys_netronome/0_ys_configure_nfp_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ls /home/opnfv/repos/yardstick/tests/opnfv/test_cases/ | xargs -Iz sed -i '/image:/s/^.*$/image: ubuntu16_nfp/' /home/opnfv/repos/yardstick/tests/opnfv/test_cases/z
-ls /home/opnfv/repos/yardstick/tests/opnfv/test_cases/ | xargs -Iz sed -i '/flavor:/s/^.*$/flavor: netronome_perf/' /home/opnfv/repos/yardstick/tests/opnfv/test_cases/z
+#ls /home/opnfv/repos/yardstick/tests/opnfv/test_cases/ | xargs -Iz sed -i '/flavor:/s/^.*$/flavor: netronome_perf/' /home/opnfv/repos/yardstick/tests/opnfv/test_cases/z
