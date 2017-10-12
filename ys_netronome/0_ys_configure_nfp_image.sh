d3889c52de07138771061d5a9eae22f314b68b99
Mario <stack@undercloud.netronome.com>
2017-10-12 05:35:10 +0000

    updated ys_config_img

--- ys_netronome/0_ys_configure_nfp_image.sh
+++ ys_netronome/0_ys_configure_nfp_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ls /home/opnfv/repos/yardstick/tests/opnfv/test_cases/ | xargs -Iz sed -i '/image:/s/^.*$/image: ubuntu16_nfp/' /home/opnfv/repos/yardstick/tests/opnfv/test_cases/z
+testcase_dir=/home/opnfv/repos/yardstick/tests/opnfv/test_cases
+
+ls $testcase_dir | xargs -Iz sed -i '/image:/s/^.*$/image: ubuntu16_nfp/' $testcase_dir/z
 #ls /home/opnfv/repos/yardstick/tests/opnfv/test_cases/ | xargs -Iz sed -i '/flavor:/s/^.*$/flavor: netronome_perf/' /home/opnfv/repos/yardstick/tests/opnfv/test_cases/z
