0576a47b436d2e22af39bd8d6599578aea60872d
Mario <stack@undercloud.netronome.com>
2017-10-12 06:10:06 +0000

    updated import_nfp

--- uc_netronome/11_import_nfp_image.sh
+++ uc_netronome/11_import_nfp_image.sh
@@ -5,4 +5,4 @@ echo "Importing $IMAGE"
 script_dir="$(dirname $(readlink -f $0))"
 glance image-create --name ubuntu16_nfp --file $script_dir/$IMAGE --disk-format qcow2 --container-format bare --progress --visibility public
 
-openstack image list | grep netronome_perf
+openstack image list | grep ubuntu16_nfp
